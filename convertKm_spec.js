2bad30fe2a2df87a060e28f4d7dafc86f18c4af7
koxxwlu <144965789+koxxwlu@users.noreply.github.com>
2025-01-24 02:04:40 +0000

    add file

--- convertKm_spec.js
+++ convertKm_spec.js
@@ -4,13 +4,13 @@ describe("test convertKiloToMiles", function() {
     it("test 150 km", function() {
         let km = 150;
         let miles = convertKmToMiles(km);
-        expect(miles.toFixed(2)).toBe(93.23);
+        expect(miles.toFixed(2)).toBe("93.23");
     });
     it("test 70 km", function() {
         let km = 70;
         let miles = convertKmToMiles(km);
-        expect(miles.toFixed(2)).toBe(93.23);
+        expect(miles.toFixed(2)).toBe("43.5");
     });
 });
 
-console.log(`${km}km is equal to ${miles}miles.`);
+// console.log(`${km}km is equal to ${miles}miles.`);
