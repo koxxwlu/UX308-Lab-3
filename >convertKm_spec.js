a1fb5045f7417d041a7ec96c0c1fab9c1f962863
koxxwlu <144965789+koxxwlu@users.noreply.github.com>
2025-01-24 01:36:58 +0000

    add new file

--- >convertKm_spec.js
+++ >convertKm_spec.js
@@ -1,3 +1,16 @@
 import {convertKmToMiles} from `./convertKiloToMiles.js`;
 
-let km = 150;
+describe("test convertKiloToMiles", function() {
+    it("test 150 km", function() {
+        let km = 150;
+        let miles = convertKmToMiles(km);
+        expect(miles.toFixed(2)).toBe(93.23);
+    });
+    it("test 70 km", function() {
+        let km = 70;
+        let miles = convertKmToMiles(km);
+        expect(miles.toFixed(2)).toBe(93.23);
+    });
+})
+
+console.log(`${km}km is equal to ${miles}miles.`)
---
+++ convertKm_spec.js
@@ -0,0 +1,9 @@
+Welcome to Node.js v20.18.1.
+Type ".help" for more information.
+> node sayThings.j
+     ^^^^^^^^^
+
+Uncaught SyntaxError: Unexpected identifier 'sayThings'
+> Uncaught ReferenceError: clear is not defined
+> Uncaught ReferenceError: clear is not defined
+> 
